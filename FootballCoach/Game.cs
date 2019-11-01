fbba19841a0f2f22212bb72a922994f1a6a4925c
Benjamin Bartel <Benjamin.Bartel@tlgclass.com>
2019-11-01 16:00:10 -0700

    Added a readiness check and "cheat" team

--- FootballCoach/Game.cs
+++ FootballCoach/Game.cs
@@ -8,10 +8,20 @@ namespace FootballCoach
     {
         public static void StartGame()
         {
+            string playerTeam = CompName();
             string compTeam = CompName();
-            Console.WriteLine("Welcome, Football Coach! \n\nPlease enter a team name: \n");
-            string playerTeam = Console.ReadLine();
-            Console.WriteLine($"\nAre you ready to lead the {playerTeam} to victory against the {compTeam}?");
+
+            while (playerTeam == compTeam)
+                playerTeam = CompName();
+            
+            Console.WriteLine($"Welcome, Coach! \n\nAre you ready to lead the {playerTeam} to victory against the {compTeam}?\n");
+
+            bool ready = true;
+
+            do
+            {
+                // main game play will go here.
+            } while (ready == true);
         }
 
         public static string CompName()
@@ -21,14 +31,15 @@ namespace FootballCoach
                                                         "Lions", "Packers", "Texans", "Colts", "Jaguars",
                                                         "Chiefs", "Chargers", "Rams", "Dolphins", "Vikings", "Patriots",
                                                         "Saints", "Giants", "Jets", "Raiders", "Eagles", "Steelers",
-                                                        "49ers", "Seahawks", "Buccaneers", "Titans", "Redskins"};
+                                                        "49ers", "Seahawks", "Buccaneers", "Titans", "Redskins", "Tibouron Sharks"};
 
             Random rand = new Random();
 
-            string team = nflTeams[rand.Next(0, 32)];
+            string team = nflTeams[rand.Next(0, 33)];
 
             return team;
         }
 
+        public static void 
     }
 }
